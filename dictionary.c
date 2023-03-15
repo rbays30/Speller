bf570df4c46315aef078f4257c8b04f5d9764de8
Levi Bays <lbays30@gmail.com>
2023-03-15 16:03:44 -0400

    Fixed Loading functionality

--- dictionary.c
+++ dictionary.c
@@ -24,7 +24,7 @@ node *table[N];
 
 //Prototypes
 
-bool create_node(node * current_node);
+bool create_node(int value);
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
@@ -45,8 +45,6 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    printf("Hi");
-
 
     FILE *input = fopen(dictionary,"r");
     if (input == NULL)
@@ -80,16 +78,17 @@ bool load(const char *dictionary)
     int count = 0;
     while (fread(buffer, sizeof(char), 1, input))
     {
-        sort_num = atoi(buffer);
         //if the last character is a first letter
-        if (last_char == 'n')
+        if (last_char == '\n')
         {
+            sort_num = (int)*buffer - 97;
+
             //Creates a new node
-            bool success = create_node(table[sort_num]);
+            bool success = create_node(sort_num);
             if(!success)
             {
                 printf("Out of Memory for new pointer.\n");
-                return 1;
+                return false;
             }
 
             count = 0;
@@ -102,32 +101,13 @@ bool load(const char *dictionary)
         count += 1;
     }
 
-    //Check to make sure words were copied
-    for (int i = 0; i < 5; i++)
-    {
-        node *printer = NULL;
-        printer = table[20];
-        printf("Word is: %s\n", printer->word);
-        i += 1;
-
-    }
-
-
-    //Check and see if there's a pointer for that letter
-
-    //If there's not, create one
-
-    //If there is, create a "next" pointer in that pointer and follow the pointer to next
-
-
-    return false;
+    return true;
 }
 
 //Creates a node
 
-bool create_node(node * current_node)
+bool create_node(int value)
 {
-    printf("Hi There\n");
     node * new_node = malloc(sizeof(node));
     if(new_node == NULL)
     {
@@ -139,10 +119,10 @@ bool create_node(node * current_node)
     new_node->next = NULL;
 
     //Set next to current node in case of collisions, otherwise will be set to NULL
-    new_node->next = current_node;
+    new_node->next = table[value];
 
     //Set the current node to be equal to the new_node pointing to the new data
-    current_node = new_node;
+    table[value] = new_node;
 
     return 1;
 }
